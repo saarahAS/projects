2806fa486b14e83c0276e23af42399eeca91dc27
saarah <saarahnadeem@hotmail.co.uk>
2023-01-27 16:59:43 +0000

    t40

--- T40/bookTest.js
+++ T40/bookTest.js
@@ -1,41 +1,42 @@
-
-let pers = [];//Create an empty array that we will use to store all the Person objects created.
+let booksArray = []; //Create an empty array that we will use to store all the Person objects created.
 
 function myLoad() {
-    let htmlSelect = document.getElementById("personList");
-    htmlSelect.style.visibility = "hidden";
+  let htmlSelect = document.getElementById("bookList");
+  htmlSelect.style.visibility = "hidden";
 
-    if (sessionStorage.getItem("hasCodeRunBefore") === null) {
-       // let arrayOfPersonObjects = [];
-        sessionStorage.setItem("persons", JSON.stringify(pers));
-        sessionStorage.setItem("hasCodeRunBefore", true);
-    } else {
-        pers = JSON.parse(sessionStorage.getItem("persons"));//Get the array of person objects from sessionStorage and assign it to the array 'pers'
-        let i = 0;
-        pers.forEach(function(p) {//Loop through each person (p) in the pers array
-            /*For each person in the array create an option element that displays 
-            that person's name and add it to the select (dropdown) element on the HTML page */
-            let optItem = document.createElement("option");
-            optItem.innerHTML = p.name.first;
-            optItem.value = i;
-            i = i + 1;
-            htmlSelect.appendChild(optItem);
-        });
-        if (i > 0) {//Only make the select element visible once there is at least one person object added that the user can select.
-            htmlSelect.style.visibility = "visible";
-        }
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+    // let arrayOfBookObjects = [];
+    sessionStorage.setItem("books", JSON.stringify(booksArray));
+    sessionStorage.setItem("hasCodeRunBefore", true);
+  } else {
+    booksArray = JSON.parse(sessionStorage.getItem("books")); //Get the array of person objects from sessionStorage and assign it to the array 'pers'
+    for (let i = 0; i < booksArray.length; i++) {
+      const book = booksArray[i];
+
+      let listItem = document.createElement("li");
+      listItem.innerHTML = `Title: ${JSON.stringify(
+        book.title
+      )}<br>Author: ${JSON.stringify(book.author)}<br>Genre: ${JSON.stringify(
+        book.genre
+      )}<br>Review: ${JSON.stringify(book.review)}<br>Rating:${JSON.stringify(
+        book.rating
+      )}<hr>`;
+      htmlSelect.appendChild(listItem);
     }
+    if (booksArray.length > 0) {
+      //Only make the select element visible once there is at least one person object added that the user can select.
+      htmlSelect.style.visibility = "visible";
+    }
+  }
 }
 
 //Below we create the constructor function that will be used to create all Person objects.
-function Person(first, last, age, gender, interests) {
-    this.name = {
-        first: first,
-        last: last
-    };
-    this.age = age;
-    this.gender = gender;
-    this.interests = interests;
+function Book(title, author, genre, review, rating) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.review = review;
+  this.rating = rating;
 }
 
 /* the function below will be called every time the user clicks on the button to add a person on the 
@@ -43,17 +44,17 @@ HTML page. Each time this happens we will retrieve the data about the person fro
 that the user has comnpleted. We call the Person constructor function and pass through all this data as
 arguments to create a new Person object. We then add the object to the array of people objects using the push method
 (pers.push). Because we want this information to be available accross page loads, we add the updated array of people to sessionStorage. */
-function addPerson() {
-    pers = JSON.parse(sessionStorage.getItem("persons"));
-    let newPerson = new Person(
-        document.getElementById("fName").value,
-        document.getElementById("lName").value,
-        document.getElementById("age").value,
-        document.getElementById("gender").value,
-        document.getElementById("interests").value
-    );
-    pers.push(newPerson);
-    sessionStorage.setItem("persons", JSON.stringify(pers));
+function addBook() {
+  book = JSON.parse(sessionStorage.getItem("books"));
+  let newBook = new Book(
+    document.getElementById("title").value,
+    document.getElementById("author").value,
+    document.getElementById("genre").value,
+    document.getElementById("review").value,
+    document.getElementById("rating").value
+  );
+  book.push(newBook);
+  sessionStorage.setItem("books", JSON.stringify(book));
 }
 
 /* When the user chooses a different person from the select (dropdown) element on the HTML page,
@@ -67,20 +68,20 @@ Have a look at the HTML page to see how this information is passed through to th
 used. 'this.value' refers to the value of the currently selected option element. Now notice in the function
 myLoad() (above) that 'i', which is the index of the object element in the pers array, is assigned to each option element that 
 we created when we added each option element to the select element as the page loaded. */
-function ChangeActiveUser(indexOfPersonObj) {
-    pers[indexOfPersonObj].bio = function() {
-        alert(
-            this.name.first +
-                " " +
-                this.name.last +
-                " is " +
-                this.age +
-                " years old. " +
-                this.name.first +
-                " likes " +
-                this.interests +
-                "."
-        );
-    };
-    pers[indexOfPersonObj].bio();
-}
+// function ChangeActiveUser(indexOfPersonObj) {
+//     booksArray[indexOfPersonObj].bio = function() {
+//         alert(
+//             this.name.first +
+//                 " " +
+//                 this.name.last +
+//                 " is " +
+//                 this.age +
+//                 " years old. " +
+//                 this.name.first +
+//                 " likes " +
+//                 this.interests +
+//                 "."
+//         );
+//     };
+//     booksArray[indexOfPersonObj].bio();
+// }
