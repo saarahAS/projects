179c5040f4cedab593e583bca95a8bb05d35433e
saarah <saarahnadeem@hotmail.co.uk>
2023-02-08 22:01:02 +0000

    item list

--- T38/main.js
+++ T38/main.js
@@ -1,16 +1,17 @@
-let htmlList = document.getElementById("myList");
+let htmlList = document.getElementById("itemList");
 
 let groceries = ["apples", "bananas", "bread", "milk"];
 
 // function to create list element and add each item to unordered list
 function displayItem(item, index) {
+  // creating list element
   let listItem = document.createElement("li");
   listItem.innerHTML = item;
-
   listItem.id = `groceryList${index}`;
-  //adding new list item as child to <ul>
+  listItem.addEventListener("click", () => strikeItem(index))
   htmlList.appendChild(listItem);
 
+  // creating span element
   let spanElement = document.createElement("span");
   spanElement.className = "close";
   spanElement.innerHTML = "\u00d7";
@@ -18,32 +19,43 @@ function displayItem(item, index) {
   listItem.appendChild(spanElement);
 }
 
+// function to delete item 1) removed item from array 2) hiding parent display
 const deleteItem = (index) => {
-  // removing item from array
-  console.log("deleting", index);
   groceries.splice(index, 1);
   let item = document.getElementById(`groceryList${index}`);
+  // comment reason
   item.remove();
+// item.style.display = "none";
 };
+
+const strikeItem = (index) => {
+  let item = document.getElementById(`groceryList${index}`);
+  item.className = item.className == "checked" ? "unchecked": "checked";
+
+}
+// for each item in the grocery list array, the displayItem function is called and a list and span item is created for it
 for (let i = 0; i < groceries.length; i++) {
   displayItem(groceries[i], i);
 }
-// updateList(){
-  let listItem = document.createElement("li");
 
-// get value of text in <input? and add it to array
-// if <input> is empty display alert to inseert item. else input text to array
-//reset input text value to ""
-// }
+// function to add item to list
+const addItem = () => {
+  let item = document.getElementById("input");
+  let itemName = item.value;
+  // if <input> tag is empty display alert to insert item
+  if (item.value == "") {
+    alert("Insert item to add to list");
+    // else add inputted text to array
+  } else {
+    groceries.push(itemName);
+    item.value = "";
+    // display the updated list
+    displayItem(groceries[groceries.length - 1], groceries.length - 1);
+  }
+};
 
-// let item1 = document.getElementById("0");
-// let item2 = document.getElementById("1");
-
-// //rewriting elements with strikethroughs
-// item1.innerHTML = item1.innerHTML.strike();
-// item2.innerHTML = item2.innerHTML.strike();
-
-// let items = document.getElementsByTagName("li");
-// for (let i = 0; i < items.length; i++) {
-//   let span = document.createElement("span" class = "close")
-// }
+const addItemOnEnter = (e) => {
+if (e.key == "Enter") {
+  addItem()
+}
+}
